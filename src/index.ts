79baf9237096b901474e2ea03767630fb836a415
Springcomp <springcomp@user.noreply.github.com>
2025-06-13 06:42:01 +0000

    _WIP: processing text one char at a time

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,44 @@
+import { Readable, Transform, TransformCallback } from 'node:stream';
+
 export function sum(left: number, right: number): number {
   return left + right;
 }
+
+export function read(): void {
+  const xml = '<root />';
+  const stream = new Stream(xml);
+  const transform = new T();
+
+  stream
+    .pipe(transform)
+    .on('data', (chunk: string) => {
+      console.log(`Received chunk: ${chunk}`);
+    })
+    .on('error', _ => {})
+    .on('finish', () => {});
+}
+
+class Stream extends Readable {
+  private chars: string[] = [];
+  constructor(text: string) {
+    super({});
+    this.chars = text.split('');
+  }
+  _read(_: number) {
+    this.push(this.chars.shift() || null);
+  }
+}
+
+class T extends Transform {
+  constructor() {
+    super({ objectMode: true });
+  }
+
+  _transform(chunk: unknown, _: BufferEncoding, next: TransformCallback): void {
+    const c = String(chunk);
+    this.push(c.toUpperCase());
+    next();
+  }
+}
+
+read();
